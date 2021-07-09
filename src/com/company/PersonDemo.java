523aa19fb2871c5a2503afd38d13fd0d0ee49204
Linda-Ju <linda.junkina@gmail.com>
2021-07-09 14:40:18 +0300

    HOMEWORK

--- src/com/company/PersonDemo.java
+++ src/com/company/PersonDemo.java
@@ -4,11 +4,12 @@ public class PersonDemo {
 
     public static void main(String[] args) {
         Person person = new Person();
-        person.setAge(5);
+        person.setAge(50);
         person.setGender("male");
         person.setHeight(180);
 
         System.out.println(person.gender);
+        System.out.println("height: " + person.height);
         person.walk();
         person.run();
         person.talk();
